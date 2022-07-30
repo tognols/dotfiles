a61be5f7711cf4d5228a26dc431dca8439de9b73
Matteo Tognolo <matteo@tognolo.it>
2022-07-30 10:10:23 +0200

    Substitued NvChad_And_Neovide w/ nvchad.custom submodule

--- NvChad_And_Neovide/custom/mappings.lua
+++
@@ -1,6 +0,0 @@
-local map = require("core.utils").map
-
-
--- Dashboard
-map("n", "<leader>fh", ":DashboardFindHistory <CR>")
-map("n", "<leader>cn", ":DashboardNewFile <CR>")
--- NvChad_And_Neovide/custom/plugins/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-    { 'williamboman/nvim-lsp-installer' },
-    { 'glepnir/dashboard-nvim'},
-}    
